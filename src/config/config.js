a5a9cb958f313e807e7647adaadbeae48ede6f8a
jadnac <jadnacouzi@gmail.com>
2023-02-10 16:25:07 +0200

    url

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,7 @@ const hostApi =
   process.env.NODE_ENV === "production" ? "http://localhost" : "";
 const portApi = process.env.NODE_ENV === "production" ? 9900 : "";
 
-
+const url = "https://www.massyve.vercel.app.com"
 const errorCatch = (error, string) => {
     if (error) {
       if (error.response) {
@@ -22,7 +22,7 @@ const errorCatch = (error, string) => {
   };
 
 async function loginUsers(data) {
-    const url = `${hostApi}${portApi ? `:${portApi}` : ``}/auth/login`
+    const url = `${url}/auth/login`
     return await axios.post(url, data).then(response => response)
     .catch(error => errorCatch(error, 'Error Login'))
   }
